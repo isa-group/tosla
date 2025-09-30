373e5d0549bdeac109052bf143caaaffa25a9c1b
marmolpen3 <marmolpen3@alum.us.es>
2025-09-30 12:03:13 +0200

    Include Alibaba ToS and refactor notebook for automated analysis of CAs with TOSLA

--- bin/tosla_validator.py
+++ bin/tosla_validator.py
@@ -1,166 +1,113 @@
 from rdflib import Graph, Namespace
-from rdflib.namespace import RDF, RDFS, DCTERMS
+from rdflib.namespace import RDF
 from pyshacl import validate
-from collections import defaultdict
 import time
-from rdflib.term import BNode
-from collections import defaultdict
+import json
 
-# Namespaces
 SH = Namespace("http://www.w3.org/ns/shacl#")
 ODRL = Namespace("http://www.w3.org/ns/odrl/2/")
 TOSL = Namespace("https://w3id.org/tosl/")
 TOSLA = Namespace("https://w3id.org/tosla/")
 
 DATA_PATH = ""
-SHACL_PATH = "agreement_shape.ttl"
-
-generated_labels = {}
-generated_names = defaultdict(int)
-
-def get_label(graph, node):
-    if node is None:
-        return "None"
-    # Si ya lo tenemos nombrado
-    if node in generated_labels:
-        return generated_labels[node]
-
-    # Si tiene rdfs:label o odrl:uid
-    label = graph.value(node, RDFS.label) or graph.value(node, ODRL.uid)
-    if label:
-        return str(label)
-
-    # Si es un URI, extraer el fragmento o el último segmento
-    if hasattr(node, 'split'):
-        uri = str(node)
-        if '#' in uri:
-            return uri.split('#')[-1]
-        elif '/' in uri:
-            return uri.rstrip('/').split('/')[-1]
-
-    # Si es un nodo anónimo o muy largo (e.g., hash)
-    if isinstance(node, BNode) or (str(node).startswith("n") and len(str(node)) > 30):
-        rdf_types = list(graph.objects(node, RDF.type))
-        typename = "Anonymous"
-        if rdf_types:
-            for t in rdf_types:
-                if str(t).startswith(str(ODRL)):
-                    typename = str(t).replace(str(ODRL), "")
-                elif str(t).startswith(str(TOSL)):
-                    typename = str(t).replace(str(TOSL), "")
-        generated_names[typename] += 1
-        name = f"{typename}_{generated_names[typename]}"
-        generated_labels[node] = name
-        return name
-
-    return str(node)
+SHACL_PATH = "../validator/agreement_shape.ttl"
 
 
-def parse_validation_results(results_graph_data):
-    graph = Graph()
-    graph.parse(data=results_graph_data, format="turtle")
+def iter_results_from_roots(rep_graph: Graph):
+    all_results = set(rep_graph.subjects(RDF.type, SH.ValidationResult))
+    detailed = set(rep_graph.objects(None, SH.detail))
+    roots = [r for r in all_results if r not in detailed]
+    visited = set()
 
-    edges = []
-    nodes = set()
+    def walk(r):
+        if r in visited:
+            return
+        visited.add(r)
+        yield r
+        for d in rep_graph.objects(r, SH.detail):
+            yield from walk(d)
 
-    for result in graph.subjects(RDF.type, SH.ValidationResult):
-        subject = graph.value(result, SH.focusNode)
-        predicate = graph.value(result, SH.resultPath)
-        obj = graph.value(result, SH.value)
+    for r in roots:
+        yield from walk(r)
 
-        if subject and predicate is not None:
-            edges.append((subject, predicate, obj))
-            nodes.update([subject, obj])
 
-    return edges, nodes, graph
+def extract_flat_violations(results_graph_data: str):
+    g = Graph()
+    g.parse(data=results_graph_data, format="turtle")
 
-def build_tree(edges):
-    tree = defaultdict(list)
-    for subj, pred, obj in edges:
-        tree[subj].append((pred, obj))
-    return tree
+    def val(res, p):
+        v = g.value(res, p)
+        return str(v) if v is not None else ""
 
-def collect_paths(node, tree, all_paths, path=None):
-    if path is None:
-        path = []
-    extended = False
-    for pred, child in tree.get(node, []):
-        extended = True
-        collect_paths(child, tree, all_paths, path + [(node, pred)])
-    if not extended:
-        all_paths.add(tuple(path + [(node, None)]))  # leaf node
+    out = []
+    seen_keys = set()
 
-def filter_unique_paths(all_paths):
-    return {
-        path for path in all_paths
-        if not any(other != path and path == other[:len(path)] for other in all_paths)
-    }
+    for r in iter_results_from_roots(g):
+        item = {
+            "severity": val(r, SH.resultSeverity),
+            "sourceShape": val(r, SH.sourceShape),
+            "sourceConstraintComponent": val(r, SH.sourceConstraintComponent),
+            "focusNode": val(r, SH.focusNode),
+            "valueNode": val(r, SH.value),
+            "resultPath": val(r, SH.resultPath),
+            "message": val(r, SH.resultMessage),
+        }
+        key = (
+            item["severity"],
+            item["sourceShape"],
+            item["sourceConstraintComponent"],
+            item["focusNode"],
+            item["valueNode"],
+            item["resultPath"],
+            item["message"],
+        )
+        if key not in seen_keys:
+            seen_keys.add(key)
+            out.append(item)
+    return out
 
-def validate_tosla_ttl(ttl_content, shacl_path):
+
+def validate_tosla_ttl(ttl_content: str, shacl_path: str):
+    """Valida el TTL contra el SHACL y devuelve dict con JSON de resultados."""
     g = Graph()
     try:
-        g.parse(data=ttl_content, format='turtle')
+        g.parse(data=ttl_content, format="turtle")
     except Exception as e:
         return {
             "syntax_valid": False,
-            "syntax_error": str(e)
+            "syntax_error": str(e),
         }
-    
-    # Si llegamos aquí, sintaxis OK
-    result = {
-        "syntax_valid": True,
-        "syntax_error": None
-    }
 
     start = time.time()
-    conforms, results_graph, _ = validate(
+    conforms, report_graph, _ = validate(
         data_graph=g,
         shacl_graph=shacl_path,
-        inference='rdfs',
+        shacl_graph_format="turtle",
+        inference="rdfs",
         serialize_report_graph=True,
         advanced=True,
         abort_on_first=False,
         meta_shacl=False,
         js=False,
-        max_recursion_depth=10
+        max_recursion_depth=10,
     )
     duration = time.time() - start
 
     result = {
-        "conforms": conforms,
-        "validation_time": duration,
-        "violations": []
+        "syntax_valid": True,
+        "conforms": bool(conforms),
+        "validation_time_sec": duration,
+        "violations": [],
     }
 
     if not conforms:
-        # parse violations
-        edges, nodes, graph = parse_validation_results(results_graph)
-        tree = build_tree(edges)
-        child_nodes = {obj for _, _, obj in edges}
-        roots = [n for n in nodes if n not in child_nodes]
-
-        all_paths = set()
-        for root in roots:
-            collect_paths(root, tree, all_paths)
-
-        unique_paths = filter_unique_paths(all_paths)
-
-        for path in sorted(unique_paths):
-            readable_path = []
-            for node, pred in path:
-                if pred is None:
-                    readable_path.append(get_label(graph, node))
-                else:
-                    readable_path.append(get_label(graph, node))
-                    readable_path.append(get_label(graph, pred))
-            result["violations"].append({
-                "path": readable_path
-            })
+        result["violations"] = extract_flat_violations(report_graph)
 
     return result
+
 
 if __name__ == "__main__":
     with open(DATA_PATH, "r", encoding="utf-8") as f:
         ttl_content = f.read()
     result = validate_tosla_ttl(ttl_content, SHACL_PATH)
-    print(result)
+    print(json.dumps(result, ensure_ascii=False, indent=2))
